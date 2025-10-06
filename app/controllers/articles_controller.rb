aee87568ff44be329153575e0a6f66c98bcde335
Sa3eedoo <mahmoudsaid246@gmail.com>
2025-10-06 15:49:34 +0300

    Add edit and update actions to ArticlesController, along with corresponding view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    
     def show
         @article = Article.find(params[:id])
     end
@@ -21,4 +22,20 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
 end
