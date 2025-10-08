d690752ce0e7949cbca3b78a496b581bd3eea1ba
Sa3eedoo <mahmoudsaid246@gmail.com>
2025-10-08 15:32:54 +0300

    Add user session management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to Alpha Blog #{@user.username}"
             redirect_to articles_path
         else
